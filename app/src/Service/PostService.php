9accc6bb7bc729a70d35989ec8a420dd5c43d0d4
Ed Finkler <coj@funkatron.com>
2015-10-26 15:08:00 -0400

    rename file to reflect class name

--- app/src/Service/PostService.php
+++ app/src/Service/PostService.php
@@ -6,22 +6,30 @@ use GraphBlog\Repository\AuthorRepository;
 use GraphBlog\Repository\CategoryRepository;
 use GraphBlog\Repository\PostRepository;
 use GraphBlog\Repository\TagRepository;
+use Psr\Log\LoggerInterface;
 
-class Post
+class PostService
 {
     public function __construct(
         PostRepository $postRepository,
         AuthorRepository $authorRepository,
         CategoryRepository $categoryRepository,
-        TagRepository $tagRepository
+        TagRepository $tagRepository,
+        LoggerInterface $logger
     )
     {
-
+        $this->postRepository = $postRepository;
+        $this->authorRepository = $authorRepository;
+        $this->categoryRepository = $categoryRepository;
+        $this->tagRepository = $tagRepository;
+        $this->logger = $logger;
     }
 
     public function getPosts() {}
 
-    public function getPost() {}
+    public function getPostBySlug($slug) {
+        return $this->postRepository->getBySlug($slug);
+    }
 
     public function getPostWithEverything() {}
 
