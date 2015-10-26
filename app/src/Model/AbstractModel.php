3404e92a238fee5e0024e6775c3d6426dbaa42b8
Ed Finkler <coj@funkatron.com>
2015-10-26 15:09:30 -0400

    can't do static abstract methods. Who knew?

--- app/src/Model/AbstractModel.php
+++ app/src/Model/AbstractModel.php
@@ -7,7 +7,15 @@ use \StdClass;
 
 abstract class AbstractModel implements \JsonSerializable
 {
-    public static abstract function fromArray(array $props);
+    /**
+     * apparently you can't have abstract static methods
+     * @param array $props
+     */
+    public static function fromArray(array $props) {}
 
-    public static abstract function fromObject(stdClass $props);
+    /**
+     * apparently you can't have abstract static methods
+     * @param StdClass $props
+     */
+    public static function fromObject(stdClass $props) {}
 }
