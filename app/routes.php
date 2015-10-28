8a5963df205736d0ab2054187274b646446ab79d
Ed Finkler <coj@funkatron.com>
2015-10-28 11:21:46 -0400

    make GET /post actually render a post as HTML

--- app/routes.php
+++ app/routes.php
@@ -4,9 +4,31 @@
 $app->get('/', 'GraphBlog\Action\HomeAction:dispatch')
     ->setName('homepage');
 
-$app->get('/post', function($request, $response, $args) {
+$app->get('/post', function ($request, $response, $args) {
     $container = $this->getContainer();
     /** @var  $postService */
     $postService = $container->get('postService');
-    var_dump($postService->getPostBySlugWithEverything('modi-aperiam-eos-eveniet-quas'));
+    $post = $postService->getPostBySlug('modi-aperiam-eos-eveniet-quas', true)->toArray();
+    // var_export($postService->getPostBySlug('modi-aperiam-eos-eveniet-quas', true)->toArray());
+    // echo "<hr>";
+    // var_dump($postService->getPosts(10, 0, true));
+
+    return $this->view->render(
+        $response,
+        'pages/post.twig',
+        compact('post')
+    );
+})->setName('get-post');
+
+$app->get('/posts', function ($request, $response, $args) {
+    $container = $this->getContainer();
+    /** @var  $postService */
+    $postService = $container->get('postService');
+    $posts = $postService->getPosts(10, 0, true);
+
+    return $this->view->render(
+        $response,
+        'pages/posts.twig',
+        $post
+    );
 })->setName('get-post');
